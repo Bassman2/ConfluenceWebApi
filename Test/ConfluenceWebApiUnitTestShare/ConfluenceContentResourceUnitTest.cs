cd5937b463aa84bed9a2a72ed73fe1116e46116b
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-05-26 16:09:26 +0200

    Update

--- Test/ConfluenceWebApiUnitTestShare/ConfluenceContentResourceUnitTest.cs
+++ Test/ConfluenceWebApiUnitTestShare/ConfluenceContentResourceUnitTest.cs
@@ -4,7 +4,7 @@
 public class ConfluenceContentResourceUnitTest : ConfluenceBaseUnitTest
 {
     [TestMethod]
-    public async Task TestMethodGetContentAsyncAsync()
+    public async Task TestMethodGetContentAsync()
     {
         using var confluence = new Confluence(storeKey, appName);
         var contents = confluence.GetContentAsync(testSpace);
@@ -13,13 +13,13 @@ public class ConfluenceContentResourceUnitTest : ConfluenceBaseUnitTest
         var list = (await contents.ToListAsync()).OrderBy(i => i!.Title).ToList();
         Assert.IsNotNull(list);
 
-        var item = list.SingleOrDefault();
+        var item = list.FirstOrDefault();
         Assert.IsNotNull(item);
 
-        Assert.AreEqual("127199830", item.Id, "Id");
+        Assert.AreEqual("209866578", item.Id, "Id");
         Assert.AreEqual(Types.Page, item.Type, "Type");
         Assert.AreEqual(Statuses.Current, item.Status, "Status");
-        Assert.AreEqual("Beckers, Ralfs Startseite", item.Title, "Title");
+        Assert.AreEqual("Abkürzungen", item.Title, "Title");
 
 
     }
