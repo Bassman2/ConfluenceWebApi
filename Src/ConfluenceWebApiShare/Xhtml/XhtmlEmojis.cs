f0d48763b39264d34f3b0d980bd84d05940ece65
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-05-30 21:57:34 +0200

    Update

---
+++ Src/ConfluenceWebApiShare/Xhtml/XhtmlEmojis.cs
@@ -0,0 +1,21 @@
+﻿namespace ConfluenceWebApi.Xhtml;
+
+public static class XhtmlEmojis
+{
+    public readonly static XhtmlText Tick = new("<ac:emoticon ac:name=\"tick\"/>");
+    public readonly static XhtmlText Cross = new("<ac:emoticon ac:name=\"cross\"/>");
+    public readonly static XhtmlText Warning = new("<ac:emoticon ac:name=\"warning\"/>");
+    public readonly static XhtmlText Question = new("<ac:emoticon ac:name=\"question\"/>");
+    public readonly static XhtmlText Plus = new("<ac:emoticon ac:name=\"plus\"/>");
+    public readonly static XhtmlText Minus = new("<ac:emoticon ac:name=\"minus\"/>");
+    public readonly static XhtmlText Information = new("<ac:emoticon ac:name=\"information\"/>");
+
+    public readonly static XhtmlText BlueStar = new("<ac:emoticon ac:name=\"blue-star\"/>");
+    public readonly static XhtmlText RedStar = new("<ac:emoticon ac:name=\"red-star\"/>");
+    public readonly static XhtmlText GreenStar = new("<ac:emoticon ac:name=\"green-star\"/>");
+
+    public readonly static XhtmlText LightOn = new("<ac:emoticon ac:name=\"light-on\"/>");
+    public readonly static XhtmlText LightOff = new("<ac:emoticon ac:name=\"light-ff\"/>");
+
+    public readonly static XhtmlText RaisedHand = new("<ac:emoticon ac:name=\"raised_hand\"/>");
+}
---
+++ Src/ConfluenceWebApiShare/Xhtml/XhtmlHeading.cs
@@ -0,0 +1,32 @@
+﻿namespace ConfluenceWebApi.Xhtml;
+
+public class XhtmlHeading1(params XhtmlElement[] elements) : XhtmlElement(elements)
+{
+    public override string ToString() => $"<h1>{ChildrenText}</h1>";
+}
+
+public class XhtmlHeading2(params XhtmlElement[] elements) : XhtmlElement(elements)
+{
+    public override string ToString() => $"<h2>{ChildrenText}</h2>";
+}
+
+public class XhtmlHeading3(params XhtmlElement[] elements) : XhtmlElement(elements)
+{
+    public override string ToString() => $"<h3>{ChildrenText}</h3>";
+}
+
+public class XhtmlHeading4(params XhtmlElement[] elements) : XhtmlElement(elements)
+{
+    public override string ToString() => $"<h4>{ChildrenText}</h4>";
+}
+
+public class XhtmlHeading5(params XhtmlElement[] elements) : XhtmlElement(elements)
+{
+    public override string ToString() => $"<h5>{ChildrenText}</h5>";
+}
+
+public class XhtmlHeading6(params XhtmlElement[] elements) : XhtmlElement(elements)
+{
+    public override string ToString() => $"<h6>{ChildrenText}</h6>";
+}
+
